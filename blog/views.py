31b755c077a7ecc6c22de26bb9a3385da5b8f8a0
Amos Omariba <amomariba@gmail.com>
2024-11-13 18:37:39 +0300

    created a post_detail.html file

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
 class BlogListView(ListView):
     model = Post
     template_name = "home.html"
+
+
+class BlogDetailView(ListView):
+    model = Post
+    template_name = "post_detail.html"
